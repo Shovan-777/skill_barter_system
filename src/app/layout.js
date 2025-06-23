d8d67cf0fc8dcaf2599870da41f2328efd4e869e
Shovan-777 <shovanshiwakoti7@gmail.com>
2025-06-23 12:00:25 +0545

    Resolved merge conflict after dark mode integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,16 +19,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-<<<<<<< HEAD
     <html lang="en" suppressHydrationWarning className="
          bg-white
          text-black
          dark:bg-gray-900
          dark:text-white
          transition-colors duration-300 min-h-screen">
-=======
-    <html lang="en" suppressHydrationWarning>
->>>>>>> 0a5ad76c326b06efedeb1d9295e022947410ef83
       <body
         className={`
           ${geistSans.variable} ${geistMono.variable} antialiased
@@ -36,16 +32,11 @@ export default function RootLayout({ children }) {
         `}
       >
         <Header />
-<<<<<<< HEAD
         <div className=" bg-white text-black
          dark:bg-gray-900 dark:text-white
         transition-colors duration-300
          min-h-screen mt-32 px-4">
           {children}
-=======
-        <div className="container mt-32 px-4">
-        {children}
->>>>>>> 0a5ad76c326b06efedeb1d9295e022947410ef83
         </div>
       </body>
     </html>
